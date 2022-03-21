e805b8065cd6262215dd7790cde86f8375b67b07
Your Name <you@example.com>
2022-03-21 17:15:51 -0500

    Grid display, modelo de producto

---
+++ Products.js
@@ -0,0 +1,103 @@
+"use strict"
+ var products= [
+    {
+        Name:"Camisa Azul",
+        Gender: "Female",
+        Img:"Img/camisa azul mujer.jpg",
+        Type: "Shirt",
+        Description: "Una camisa para el dia a dia"
+    },
+    {
+        Name:"polo negro",
+        Gender: "male",
+        Img:"Img/polo negro hombre.jpg",
+        Type: "Shirt",
+        Description: "Una camisa para el dia a dia"
+    },
+    {
+        Name:"Camisa Azul oscuro",
+        Gender: "male",
+        Img:"Img/camisa azul oscuro hombre.jpg",
+        Type: "Shirt",
+        Description: "Una camisa para el dia a dia"
+    },
+    {
+        Name:"Camisa negra",
+        Gender: "female",
+        Img:"Img/camisa negra mujer.jpg",
+        Type: "Shirt",
+        Description: "Una camisa para el dia a dia"
+    },
+    {
+        Name:"Camisa Azul",
+        Gender: "male",
+        Img:"Img/camisa-oxford-azul.jpg",
+        Type: "Shirt",
+        Description: "Una camisa para el dia a dia"
+    },
+    {
+        Name:"Chaqueta cuero",
+        Gender: "male",
+        Img:"Img/chaqueta cuero hombre.jpg",
+        Type: "Jacket",
+        Description: "Una chaqueta de cuero para el dia a dia",
+    },
+    {
+        Name:"Chaqueta cuero negro",
+        Gender: "female",
+        Img:"Img/Chaqueta cuero negro mujer.jpg",
+        Type: "Jacket",
+        Description: "Una chaqueta para el dia a dia"
+    },
+    {
+        Name:"Chaqueta jean",
+        Gender: "female",
+        Img:"Img/Chaqueta jean mujer.jpg",
+        Type: "Jacket",
+        Description: "Una chaqueta para el dia a dia"
+    },
+    {
+        Name:"chompa cuero cafe",
+        Gender: "male",
+        Img:"Img/chompa cuero cafe hombre.jpg",
+        Type: "Jacket",
+        Description: "Una chompa para el dia a dia"
+    },
+    {
+        Name:"Gaban cuero negro",
+        Gender: "female",
+        Img:"Img/Gaban cuero negro mujer.jpg",
+        Type: "Gaban",
+        Description: "Un gaban para el dia a dia"
+    },
+    {
+        Name:"Jean azul claro tiro alto",
+        Gender: "female",
+        Img:"Img/jean azul claro tiro alto mujer.jpg",
+        Type: "Jean",
+        Description: "Un jean para el dia a dia"
+    },
+    {
+        Name:"Jean azul oscuro",
+        Gender: "male",
+        Img:"Img/jean azul oscuro hombre.jpg",
+        Type: "Jean",
+        Description: "Un jean para el dia a dia"
+    },
+    {
+        Name:"Jean negro",
+        Gender: "male",
+        Img:"Img/jean negro hombre.jpg",
+        Type: "Jean",
+        Description: "Un jean para el dia a dia"
+    },
+    {
+        Name:"pantalon cafe claro bota ancha",
+        Gender: "female",
+        Img:"Img/pantalon cafe claro bota ancha mujer.jpg",
+        Type: "Pant",
+        Description: "Un pantalon para el dia a dia"
+    }
+
+    
+];
--- ScriptIndex.js
+++ ScriptIndex.js
@@ -1,1 +1,13 @@
 "use strict"
+
+var productSection = document.getElementById("Products");
+products.forEach(item => {
+    let Html = `
+            <div>
+                <h3>${item.Name}</h3>
+                <img src="${item.Img}">
+                <h4>${item.Type}</h4>
+                <p>${item.Description}</p>
+            </div>`;
+    productSection.innerHTML+= Html;
+});
