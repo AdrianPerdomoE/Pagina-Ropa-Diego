40d99dd0b43042b9a62a5ca5f28e05b25fbbd899
Your Name <you@example.com>
2022-03-21 20:36:18 -0500

    Lightbox implementation

--- ScriptIndex.js
+++ ScriptIndex.js
@@ -1,5 +1,7 @@
 "use strict"
-
+/* Esta seccion se encarga de cargar los productos de la lista de productos y organizar en la platilla
+para mostrarlos ordenados en pantalla
+*/
 var productSection = document.getElementById("Products");
 products.forEach(item => {
     let Html = `
@@ -11,3 +13,27 @@ products.forEach(item => {
             </div>`;
     productSection.innerHTML+= Html;
 });
+//esta seccion es la parte logica del Lightbox
+const LIGHTBOX = document.createElement("div");
+LIGHTBOX.id = "lightbox";
+document.body.appendChild(LIGHTBOX);
+const IMAGES = document.querySelectorAll("img");
+IMAGES.forEach(image => {
+    image.addEventListener("click",event=>{
+        LIGHTBOX.classList.add("active");
+        const IMG = document.createElement("img");
+        IMG.src = image.src;
+        while (LIGHTBOX.firstChild){
+            LIGHTBOX.removeChild(LIGHTBOX.firstChild);
+        }
+        LIGHTBOX.appendChild(IMG);
+    });
+});
+LIGHTBOX.addEventListener("click",event=>{
+   if(event.target !== event.currentTarget){
+       return;
+   }
+
+    LIGHTBOX.classList.remove("active");
+
+})
